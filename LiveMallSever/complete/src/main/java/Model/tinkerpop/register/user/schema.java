6840d70cbb3d84a318502db100cf77a8d8d8317b
wjl198435 <jinliang.wang@tongdun.cn>
2017-09-07 11:11:40 +0800

    修改变量命名

--- LiveMallSever/complete/src/main/java/Model/tinkerpop/register/user/schema.java
+++ LiveMallSever/complete/src/main/java/Model/tinkerpop/register/user/schema.java
@@ -70,7 +70,7 @@ public  class schema {
 
 
 
-    static private  String[] wxUserInfoArray ={reg_channel,wx_account,wx_openId,wx_unionId,
+    static private  String[] wxUserInfoPropertyKeyArray ={reg_channel,wx_account,wx_openId,wx_unionId,
             wx_nickName,wx_avatarUrl,wx_gender,wx_country,wx_province, wx_city};
 
 
@@ -107,108 +107,27 @@ public  class schema {
     //会员等级
     static private final String userLevel="userLevel";
 
+    
 
 
-
-    public static void makeVertexLabel(JanusGraph jGraph ,String vertexLabel ,Boolean partition ) {
-
-
-        if (jGraph != null && !vertexLabel.isEmpty()) {
-            JanusGraphManagement  mgmt  = jGraph.openManagement();
-            if (mgmt != null) {
-
-                if (mgmt.getVertexLabel(vertexLabel) == null) {
-                    LOGGER.info("makeVertexLabel :" + vertexLabel + "as not existing");
-
-                    if (partition == true) {
-                        VertexLabel label = mgmt.makeVertexLabel(vertexLabel).partition().make();
-                    }
-                    else {
-                        VertexLabel label = mgmt.makeVertexLabel(vertexLabel).make();
-                    }
-
-                    LOGGER.info("makeVertexLabel :" + vertexLabel + "is ok");
-                }
-
-                mgmt.commit();
-
-            }
-
-        } else {
-            LOGGER.info("makeVertexLabel :" + vertexLabel + " is exsiting ,Nothing to do");
-        }
-    }
-
-
-
-    public static void makeEdgeLabel(JanusGraph jGraph, String edgeLabelLabel, String sig ,Multiplicity multi ) {
-
-        LOGGER.info(" Enter makeVertexPropertyKeyIndex create edgeLabelLabel: " + edgeLabelLabel + "--sig:" + sig + "--multi:" + multi);
-
-        if (jGraph != null && !edgeLabelLabel.isEmpty()) {
-            JanusGraphManagement mgmt  = jGraph.openManagement();
-            if (mgmt != null) {
-                if (!mgmt.containsEdgeLabel(edgeLabelLabel)) {
-                    if (sig != null) {
-                        LOGGER.info("makeEdgeLabel :" + edgeLabelLabel + " as not existing" + "signature:" + sig + " Multiplicity:" + multi);
-                        PropertyKey sigPropertyKey  = mgmt.getOrCreatePropertyKey(sig);
-                        EdgeLabel edgeLabel = mgmt.makeEdgeLabel(edgeLabelLabel).multiplicity(multi).signature(sigPropertyKey).make();
-                        mgmt.buildEdgeIndex(edgeLabel, "battlesByTime", Direction.BOTH, Order.decr, sigPropertyKey);
-                        LOGGER.info("makeEdgeLabel :" + edgeLabelLabel + " is ok");
-                    }
-                    else {
-                        EdgeLabel edgeLabel = mgmt.makeEdgeLabel(edgeLabelLabel).multiplicity(multi).make();
-                    }
-                }
-                else {
-                    LOGGER.info("makeEdgeLabel:" + edgeLabelLabel + " is exsiting");
-                }
-                mgmt.commit();
-            }
-        }
-    }
-
-
-    void  makePropertyKey(JanusGraph jGraph,String propertyKey,Class<?> classType,Cardinality cardinality ){
-        if (jGraph != null && !propertyKey.isEmpty()) {
-            JanusGraphManagement mgmt = jGraph.openManagement();
-            if (mgmt != null) {
-                if (!mgmt.containsPropertyKey(propertyKey)) {
-                    LOGGER.info("makePropertyKey:" + propertyKey + " as not existing");
-                    final PropertyKey property = mgmt.makePropertyKey(propertyKey).dataType(classType).cardinality(cardinality).make();
-                    LOGGER.info("makePropertyKey:" + propertyKey +":dataType:"+ String.class+" ...is ok");
-                }
-                mgmt.commit();
-            }
-        }
-    }
-
-
-
-//    public static void main( String[] args )
-//    {
-//        for(int i=0;i<wxUserInfoArray.length;i++){
-//            LOGGER.info(wxUserInfoArray[i]);
-//        }
-//    }
 
 
 
     public static void main(String[] args) {
 
-        for(int i=0;i<wxUserInfoArray.length;i++){
+        for(int i=0;i<wxUserInfoPropertyKeyArray.length;i++){
             //LOGGER.info(wxUserInfoArray[i]);
 
-            switch(wxUserInfoArray[i])
+            switch(wxUserInfoPropertyKeyArray[i])
             {
                 case reg_channel :
                     {
-                        LOGGER.info("reg_channel:"+wxUserInfoArray[i]);
+                        LOGGER.info("reg_channel:"+wxUserInfoPropertyKeyArray[i]);
                     break;
                    }
 
 
-                default:LOGGER.info(wxUserInfoArray[i]);
+                default:LOGGER.info(wxUserInfoPropertyKeyArray[i]);
             }
 
         }
